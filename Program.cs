b2ee9fc716e243a0f107beb48e997c926e3b56ca
Andrey Rodin <andrey.val.rodin@gmail.com>
2024-11-15 13:39:58 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using ExcelDataReader;
+using WarehouseFiller;
+
+System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+var provider = new SqlProvider("../../../Components.db");
+
+using (var stream = File.Open("../../../Components.xlsx", FileMode.Open, FileAccess.Read))
+{
+    using var reader = ExcelReaderFactory.CreateReader(stream);
+
+    // Skip header
+    reader.Read();
+
+    while (reader.Read())
+    {
+        if (reader.GetValue(0) == null)
+            continue;
+
+        int componentId = (int)reader.GetDouble(0);
+        var name = reader.GetString(1);
+        int type = (int)reader.GetDouble(2);
+
+        provider.InsertComponent(componentId, name, type);
+
+        for (int i = 3, productId = 1; i <= 22; i++, productId++)
+        {
+            if (reader.GetValue(i) == null)
+                continue;
+
+            int amount = (int)reader.GetDouble(i);
+            provider.InsertProductComponent(productId, componentId, amount);
+        }
+    }
+}
---
+++ SqlProvider.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+using System.Data.SQLite;
+using System.Xml.Linq;
+
+namespace WarehouseFiller
+{
+    internal class SqlProvider
+    {
+        private SQLiteConnection _connection;
+
+        public SqlProvider(string path)
+        {
+            _connection = new SQLiteConnection($"DataSource={path};Mode=ReadWrite");
+            _connection.Open();
+        }
+
+        public void InsertComponent(int id, string name, int type)
+        {
+            var query = @"
+INSERT INTO Component (Id, Name, Type)
+VALUES
+(@id, @name, @type)";
+            using var command = new SQLiteCommand(query, _connection);
+            command.Parameters.Add(new SQLiteParameter("@id", id));
+            command.Parameters.Add(new SQLiteParameter("@name", name));
+            command.Parameters.Add(new SQLiteParameter("@type", type));
+            command.ExecuteNonQuery();
+        }
+
+        public void InsertProductComponent(int productId, int componentId, int amount)
+        {
+            var query = @"
+INSERT INTO ProductComponent (ProductId, ComponentId, Amount)
+VALUES
+(@productId, @componentId, @amount)";
+            using var command = new SQLiteCommand(query, _connection);
+            command.Parameters.Add(new SQLiteParameter("@productId", productId));
+            command.Parameters.Add(new SQLiteParameter("@componentId", componentId));
+            command.Parameters.Add(new SQLiteParameter("@amount", amount));
+            command.ExecuteNonQuery();
+        }
+    }
+}
